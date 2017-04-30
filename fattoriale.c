32ac4b256f4f359f27d49ae5ddecf562dc89b0cb
Alessandro Sanino <AlessandroSanino1994@users.noreply.github.com>
2017-04-30 15:49:36 +0200

    Update fattoriale.c

--- fattoriale.c
+++ fattoriale.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+//short way to implement (no dinamic programming feature added
+/*
+long int fattoriale(int n) {
+    return n == 0 ? 1 : fattoriale(n-1) * n;	
+}
+*/
+
 long int fattoriale(int n){
 	if (n==0){
 		return 1;
